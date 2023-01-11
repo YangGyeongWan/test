533494d48d1efc3900a27b6bf03c9a8dc05ed248
Gyeongwan Yang <102352262+YangGyeongWan@users.noreply.github.com>
2023-01-11 19:33:05 +0900

    Question_No.2
    체이닝 메소드를 이용한 add,substract 함수 구현 및 출력

---
+++ Question_No2/ChainingCal.java
@@ -0,0 +1,31 @@
+package vcanus.programmingtest.q2;
+
+public class ChainingCal {
+	
+	private int add1;
+	private int add2;
+	private int substract;
+	
+	public int getAdd1() {
+		return add1;
+	}
+	public ChainingCal setAdd1(int add1) {
+		this.add1 = add1;
+		return this;
+	}
+	public int getAdd2() {
+		return add2;
+	}
+	public ChainingCal setAdd2(int add2) {
+		this.add2 = add2;
+		return this;
+	}
+	public int getSubstract() {
+		return substract;
+	}
+	public ChainingCal setSubstract(int substract) {
+		this.substract = substract;
+		return this;
+	}
+	
+}
